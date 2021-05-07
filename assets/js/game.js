68b9d95bc9ea592d0ed238f9dc6cbc176ffac6e4
Mike <mcewen1984@hotmail.com>
2021-05-06 20:57:47 -0700

    initial thoughts

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+// store questions and answers in an object array
+
+// start button start the quiz
+// <main> changes to show first question and possible answers
+// 
+// store correct answer inside of object? or all the possible answers and select and if statement to see if it's correct.
+
+// If user selects right answer increase store, and alert them they answered correctly and move on to the next question.
+// If they answer incorrectly, then alert them they answered wrong, time is deducted from their time left and move on to the next question.
+// once they answer all the questions it will show them their score and ask them to enter their initials.
+// it their score is higher than the high score, then it will tell them they are the new high scorer and put their initials on the screen
+
+// when the user clicks the high scores link it shows them the last 5 high scores
+
+
+
+
